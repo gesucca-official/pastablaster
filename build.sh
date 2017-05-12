f8c446c37e8dd2ac387ec0076629283bce4b0554
gesucca <sim.cipr@gmail.com>
2017-05-12 14:53:05 +0200

    Started exploring SFML, done something!

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ rm -rf bin
 mkdir bin
 
 g++ -c src/main.cpp -o bin/main.o
-g++ bin/main.o -o bin/duelgraph -lsfml-graphics -lsfml-window -lsfml-system
+g++ bin/main.o -o bin/duelgraph -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system
 
 ./bin/duelgraph
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,101 @@
+#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+int main() {
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	// Create the main world (random resolution for now)
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Duel Screen", sf::Style::Fullscreen);
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	// create a view centered in 0,0, res 800*600
+	sf::View view(sf::FloatRect(0, 0, 800, 600));
 
-    return 0;
+	// activate it
+	window.setView(view);
+
+	// Load a sprite to display
+	sf::Texture texture;
+	if (!texture.loadFromFile("./img/Mario_Nintendo.png"))
+		return EXIT_FAILURE;
+	sf::Sprite sprite(texture);
+	sprite.move(200, 50);
+
+	// Create a graphical text to display
+	sf::Font font;
+	if (!font.loadFromFile("./fnt/blk_dmd.ttf"))
+		return EXIT_FAILURE;
+	sf::Text text("This thing works!", font, 50);
+	text.move(300, 400);
+
+	// Create a green circle for its own sake
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	// Load a music to play
+	sf::Music music;
+	if (!music.openFromFile("./sng/song.ogg"))
+		return EXIT_FAILURE;
+	music.setLoop(true);
+
+	// Start the game loop
+	while (window.isOpen()) {
+
+		 // Process events
+		sf::Event event;
+		while (window.pollEvent(event)) {
+
+			// LEFT CLICK: Play the music
+			if (event.type == sf::Event::MouseButtonPressed) {
+				if (event.mouseButton.button == sf::Mouse::Left) 
+					music.play();
+				if (event.mouseButton.button == sf::Mouse::Right) 
+					music.pause();
+			}
+
+			// ESC BUTTON: exit
+			if (event.type == sf::Event::KeyPressed) {
+				if (event.key.code == sf::Keyboard::Escape)
+					window.close();
+
+
+			// WASD: move the big Mario
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+					sprite.move(0, -3);
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+					sprite.move(-3, 0);
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+					sprite.move(0, 3);
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+					sprite.move(3, 0);
+
+			// ARROWS: adjust view
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+					view.move(0, -2);
+					window.setView(view);
+				}
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+					view.move(0, 2);
+					window.setView(view);
+				}
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+					view.move(-2, 0);
+					window.setView(view);
+				}
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+					view.move(2, 0);
+					window.setView(view);
+				}
+			}
+		}
+
+		window.clear();
+
+		// Draw stuff
+		window.draw(sprite);
+		window.draw(text);
+		window.draw(shape);
+
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
 }
